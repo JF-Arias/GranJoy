82ddc1c336433c30f675d0116fc8ece6720f86f5
JF-Arias <juanfelipeariasgarcia@gmail.com>
2025-03-19 07:21:45 -0500

    coreccion backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,26 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const pool = require('./db');
 const app = express();
 const port = 3000;
 const authRoutes = require('./routes/authRoutes');
 
+// Habilita CORS para que el frontend pueda comunicarse con el backend
+app.use(cors());
 
-
+// Middleware para parsear JSON
 app.use(express.json());
 
 // Rutas
 app.use('/auth', authRoutes);
-
-// Ruta de prueba de conexión
-//app.get('/test', async (req, res) => {
-//    try {
-//    const [rows] = await pool.query('SELECT 1 + 1 AS result');
-//    res.json({ message: 'Conexión exitosa', result: rows[0].result });
-//    } catch (error) {
-//    res.status(500).json({ message: 'Error de conexión', error });
-//    }
-//});
 
 // Inicia el servidor
 app.listen(port, () => {
