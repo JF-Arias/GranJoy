20e159faaa87fa9165591ce8882a0ada4975251a
JF-Arias <juanfelipeariasgarcia@gmail.com>
2025-03-27 10:37:54 -0500

    correcion tokens

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,21 +1,19 @@
 const jwt = require('jsonwebtoken');
 
-const authMiddleware = (req, res, next) => {
-    const token = req.header('Authorization');
+module.exports = (req, res, next) => {
+    const authHeader = req.headers.authorization;
 
-    if (!token) {
-        return res.status(401).json({ message: 'Acceso denegado. No hay token.' });
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Acceso denegado. Token no proporcionado.' });
     }
+
+    const token = authHeader.split(' ')[1];
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
+        req.user = decoded; // Asegurar que req.user tiene el ID del usuario
         next();
     } catch (error) {
-        res.status(401).json({ message: 'Token inválido.' });
+        return res.status(401).json({ message: 'Token inválido' });
     }
 };
-
-module.exports = authMiddleware;
-
-
